94f8c7ca562c4d2ee0418cde2f0f387aafa461a7
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2025-07-05 20:00:05 +0300

    ShowLogicErrors_V1

---
+++ Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Lists/Listing_01_06_ShowLogicErrors_V1.java
@@ -0,0 +1,27 @@
+/*
+ * Author: Seif Yehia
+ * Socials: SeifYehiaDev (Facebook, LinkedIn, Instagram, GitHub)
+ * Created: 05/07/2025
+ * Purpose: Java learning journey — practice from book and course
+ */
+
+package Chapter_01_Lists;
+
+public class Listing_01_06_ShowLogicErrors_V1 {
+    public static void main(String[] args) {
+        System.out.println("Celsius 35 is Fahrenheit degree");
+        System.out.println(9 / 5 * 35 + 32);
+        System.out.println("After correct the logic error....");
+        System.out.println("Celsius 35 is Fahrenheit degree");
+        System.out.print(9.0 / 5 * 35 + 32);
+    }
+}
+
+//                                                 _Output_
+/*
+Celsius 35 is Fahrenheit degree
+67
+After correct the logic error....
+Celsius 35 is Fahrenheit degree
+95.0
+ */
