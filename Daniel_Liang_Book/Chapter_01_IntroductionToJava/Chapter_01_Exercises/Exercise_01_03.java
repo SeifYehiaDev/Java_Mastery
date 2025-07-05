e03b388b343b25553dfe06c526125fb67a90d6d1
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2025-07-05 20:49:20 +0300

    Display a pattern

---
+++ Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_03.java
@@ -0,0 +1,26 @@
+/*
+ * Author: Seif Yehia
+ * Socials: SeifYehiaDev (Facebook, LinkedIn, Instagram, GitHub)
+ * Created: 05/07/2025
+ * Purpose: Java learning journey — practice from book and course
+ */
+
+package Chapter_01_Exercises;
+
+public class Exercise_01_03 {
+    public static void main(String[] args) {
+        System.out.print("""              
+                    J    A   V     V  A
+                    J   A A   V   V  A A
+                J   J  AAAAA   V V  AAAAA
+                 J J  A     A   V  A     A""");
+    }
+}
+
+//                                                 _Output_
+/*
+    J    A   V     V  A
+    J   A A   V   V  A A
+J   J  AAAAA   V V  AAAAA
+ J J  A     A   V  A     A
+ */
