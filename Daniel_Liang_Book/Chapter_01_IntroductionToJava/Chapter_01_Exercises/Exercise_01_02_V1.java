8589b0c9732c3cf9b058c1d0451f394db6908364
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2025-07-05 20:39:52 +0300

    Display five messages

---
+++ Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_02_V1.java
@@ -0,0 +1,28 @@
+/*
+ * Author: Seif Yehia
+ * Socials: SeifYehiaDev (Facebook, LinkedIn, Instagram, GitHub)
+ * Created: 05/07/2025
+ * Purpose: Java learning journey — practice from book and course
+ */
+
+package Chapter_01_Exercises;
+
+public class Exercise_01_02_V1 {
+    public static void main(String[] args) {
+        System.out.print("""
+                Welcome to Java.
+                Welcome to Java.
+                Welcome to Java.
+                Welcome to Java.
+                Welcome to Java.""");
+    }
+}
+
+//                                                 _Output_
+/*
+Welcome to Java.
+Welcome to Java.
+Welcome to Java.
+Welcome to Java.
+Welcome to Java.
+ */
---
+++ Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_02_V2.java
@@ -0,0 +1,24 @@
+/*
+ * Author: Seif Yehia
+ * Socials: SeifYehiaDev (Facebook, LinkedIn, Instagram, GitHub)
+ * Created: 05/07/2025
+ * Purpose: Java learning journey — practice from book and course
+ */
+
+package Chapter_01_Exercises;
+
+public class Exercise_01_02_V2 {
+    public static void main(String[] args) {
+        for (int index = 0; index < 5; index++)
+            System.out.println("Welcome to Java");
+    }
+}
+
+//                                                 _Output_
+/*
+Welcome to Java.
+Welcome to Java.
+Welcome to Java.
+Welcome to Java.
+Welcome to Java.
+ */
