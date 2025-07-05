f323c2b874dd835d14945e108e34e52f6040c021
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2025-07-05 20:41:50 +0300

    Display five messages

--- Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_02_V2.java
+++ Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_02_V2.java
@@ -9,16 +9,18 @@ package Chapter_01_Exercises;
 
 public class Exercise_01_02_V2 {
     public static void main(String[] args) {
-        for (int index = 0; index < 5; index++)
-            System.out.println("Welcome to Java");
+        for (int index = 0; index < 5; index++) {
+            System.out.print("Welcome to Java");
+            if (index < 4) System.out.println();
+        }
     }
 }
 
 //                                                 _Output_
 /*
-Welcome to Java.
-Welcome to Java.
-Welcome to Java.
-Welcome to Java.
-Welcome to Java.
+Welcome to Java
+Welcome to Java
+Welcome to Java
+Welcome to Java
+Welcome to Java
  */
