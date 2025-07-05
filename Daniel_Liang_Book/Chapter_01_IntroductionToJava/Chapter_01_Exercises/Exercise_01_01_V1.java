918eb402b05e011af9627bde645b52f77c791cb4
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2025-07-05 20:34:04 +0300

    Display three messages

---
+++ Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_01_V1.java
@@ -0,0 +1,23 @@
+/*
+ * Author: Seif Yehia
+ * Socials: SeifYehiaDev (Facebook, LinkedIn, Instagram, GitHub)
+ * Created: 05/07/2025
+ * Purpose: Java learning journey — practice from book and course
+ */
+
+package Chapter_01_Exercises;
+
+public class Exercise_01_01_V1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java.");
+        System.out.println("Welcome to Computer Science.");
+        System.out.print("Programming is fun.");
+    }
+}
+
+//                                                 _Output_
+/*
+Welcome to Java.
+Welcome to Computer Science.
+Programming is fun.
+ */
---
+++ Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_01_V2.java
@@ -0,0 +1,24 @@
+/*
+ * Author: Seif Yehia
+ * Socials: SeifYehiaDev (Facebook, LinkedIn, Instagram, GitHub)
+ * Created: 05/07/2025
+ * Purpose: Java learning journey — practice from book and course
+ */
+
+package Chapter_01_Exercises;
+
+public class Exercise_01_01_V2 {
+    public static void main(String[] args) {
+        System.out.print("""
+                Welcome to Java.
+                Welcome to Computer Science.
+                Programming is fun.""");
+    }
+}
+
+//                                                 _Output_
+/*
+Welcome to Java.
+Welcome to Computer Science.
+Programming is fun.
+ */
