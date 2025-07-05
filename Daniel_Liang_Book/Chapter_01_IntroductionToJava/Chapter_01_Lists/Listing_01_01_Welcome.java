3d1e021c9f58efe11931888a8c6b8db91c82e301
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2025-07-05 19:57:09 +0300

    Welcome

---
+++ Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Lists/Listing_01_01_Welcome.java
@@ -0,0 +1,19 @@
+/*
+ * Author: Seif Yehia
+ * Socials: SeifYehiaDev (Facebook, LinkedIn, Instagram, GitHub)
+ * Created: 00/00/2025
+ * Purpose: Java learning journey — practice from book and course
+ */
+
+package Chapter_01_Lists;
+
+public class Listing_01_01_Welcome {
+    public static void main(String[] args) {
+        System.out.print("Welcome to Java!");
+    }
+}
+
+//                                                 _Output_
+/*
+Welcome to Java!
+ */
