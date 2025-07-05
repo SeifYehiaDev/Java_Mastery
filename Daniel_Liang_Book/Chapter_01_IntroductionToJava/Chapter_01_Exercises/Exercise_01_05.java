16c6b746ca78d8ea3ac30becc92eb082a3056f6c
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2025-07-05 22:47:34 +0300

    Compute expressions

---
+++ Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_05.java
@@ -0,0 +1,19 @@
+/*
+ * Author: Seif Yehia
+ * Socials: SeifYehiaDev (Facebook, LinkedIn, Instagram, GitHub)
+ * Created: 05/07/2025
+ * Purpose: Java learning journey — practice from book and course
+ */
+
+package Chapter_01_Exercises;
+
+public class Exercise_01_05 {
+    public static void main(String[] args) {
+        System.out.print((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+    }
+}
+
+//                                                 _Output_
+/*
+0.8392857142857143
+ */
