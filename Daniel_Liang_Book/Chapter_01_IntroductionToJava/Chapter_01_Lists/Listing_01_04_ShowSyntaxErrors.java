ffa5407d771823c50f1622a7b557da7fd1ccfc81
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2025-07-05 19:59:45 +0300

    ShowSyntaxErrors

---
+++ Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Lists/Listing_01_04_ShowSyntaxErrors.java
@@ -0,0 +1,20 @@
+/*
+ * Author: Seif Yehia
+ * Socials: SeifYehiaDev (Facebook, LinkedIn, Instagram, GitHub)
+ * Created: 05/07/2025
+ * Purpose: Java learning journey — practice from book and course
+ */
+
+package Chapter_01_Lists;
+
+public class Listing_01_04_ShowSyntaxErrors {
+    public static void main(String[] args) {
+        // System.out.print("Welcome to Java);
+        System.out.print("Syntax error: \"java: unclosed string literal\"");
+    }
+}
+
+//                                                 _Output_
+/*
+Syntax error: "java: unclosed string literal"
+ */
