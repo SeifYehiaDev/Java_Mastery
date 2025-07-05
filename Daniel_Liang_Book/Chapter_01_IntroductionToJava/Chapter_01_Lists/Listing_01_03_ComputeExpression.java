eda4d27bc47291a0a1edb0e55fc8f95c129552aa
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2025-07-05 19:59:32 +0300

    ComputeExpression

---
+++ Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Lists/Listing_01_03_ComputeExpression.java
@@ -0,0 +1,19 @@
+/*
+ * Author: Seif Yehia
+ * Socials: SeifYehiaDev (Facebook, LinkedIn, Instagram, GitHub)
+ * Created: 05/07/2025
+ * Purpose: Java learning journey — practice from book and course
+ */
+
+package Chapter_01_Lists;
+
+public class Listing_01_03_ComputeExpression {
+    public static void main(String[] args) {
+        System.out.print((10.5 + 2 * 3) / (45 - 3.5));
+    }
+}
+
+//                                                 _Output_
+/*
+0.39759036144578314
+ */
