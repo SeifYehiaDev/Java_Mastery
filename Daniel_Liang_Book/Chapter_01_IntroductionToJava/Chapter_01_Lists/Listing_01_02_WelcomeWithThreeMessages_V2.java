5f372d55e923c952e6e58fd13c2745ede7b2ffaf
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2025-07-05 19:57:32 +0300

    WelcomeWithThreeMessages_V2

---
+++ Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Lists/Listing_01_02_WelcomeWithThreeMessages_V2.java
@@ -0,0 +1,24 @@
+/*
+ * Author: Seif Yehia
+ * Socials: SeifYehiaDev (Facebook, LinkedIn, Instagram, GitHub)
+ * Created: 00/00/2025
+ * Purpose: Java learning journey — practice from book and course
+ */
+
+package Chapter_01_Lists;
+
+public class Listing_01_02_WelcomeWithThreeMessages_V2 {
+    public static void main(String[] args) {
+        System.out.print("""
+                Programming is fun!
+                Fundamentals First
+                Problem Driven""");
+    }
+}
+
+//                                                 _Output_
+/*
+Programming is fun!
+Fundamentals First
+Problem Driven
+ */
