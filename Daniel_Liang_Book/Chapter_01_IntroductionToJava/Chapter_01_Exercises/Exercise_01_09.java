12b24dc0428aeba0a9491eef0a8ab94b2949b692
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2025-07-05 23:41:48 +0300

    Area and perimeter of a rectangle

---
+++ Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_09.java
@@ -0,0 +1,24 @@
+/*
+ * Author: Seif Yehia
+ * Socials: SeifYehiaDev (Facebook, LinkedIn, Instagram, GitHub)
+ * Created: 05/07/2025
+ * Purpose: Java learning journey — practice from book and course
+ */
+
+package Chapter_01_Exercises;
+
+public class Exercise_01_09 {
+    public static void main(String[] args) {
+        final double WIDTH = 4.5, HEIGHT = 7.9;
+        System.out.printf("""
+                        The perimeter of a rectangle is %.1f
+                        The area of a rectangle is %.2f""",
+                2 * (WIDTH + HEIGHT), WIDTH * HEIGHT);
+    }
+}
+
+//                                                 _Output_
+/*
+The perimeter of a rectangle is 24.8
+The area of a rectangle is 35.55
+ */
