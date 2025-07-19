14c351cfcd8801350acdf0250d12b7aedaead46e
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2025-07-19 20:25:09 +0300

    Display five messages

--- Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_02_V1.java
+++ Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_02_V1.java
@@ -2,7 +2,6 @@
  * Author: Seif Yehia
  * Socials: SeifYehiaDev (Facebook, LinkedIn, Instagram, GitHub)
  * Created: 05/07/2025
- * Purpose: Java learning journey — practice from book and course
  */
 
 package Chapter_01_Exercises;
@@ -10,19 +9,19 @@ package Chapter_01_Exercises;
 public class Exercise_01_02_V1 {
     public static void main(String[] args) {
         System.out.print("""
-                Welcome to Java.
-                Welcome to Java.
-                Welcome to Java.
-                Welcome to Java.
-                Welcome to Java.""");
+                Welcome to Java
+                Welcome to Java
+                Welcome to Java
+                Welcome to Java
+                Welcome to Java""");
     }
 }
 
 //                                                 _Output_
 /*
-Welcome to Java.
-Welcome to Java.
-Welcome to Java.
-Welcome to Java.
-Welcome to Java.
+Welcome to Java
+Welcome to Java
+Welcome to Java
+Welcome to Java
+Welcome to Java
  */
--- Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_02_V2.java
+++ Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_02_V2.java
@@ -2,25 +2,21 @@
  * Author: Seif Yehia
  * Socials: SeifYehiaDev (Facebook, LinkedIn, Instagram, GitHub)
  * Created: 05/07/2025
- * Purpose: Java learning journey — practice from book and course
  */
 
 package Chapter_01_Exercises;
 
 public class Exercise_01_02_V2 {
     public static void main(String[] args) {
-        for (int index = 0; index < 5; index++) {
-            System.out.print("Welcome to Java.");
-            if (index < 4) System.out.println();
-        }
+        for (int index = 0; index < 5; index++) System.out.print("Welcome to Java");
     }
 }
 
 //                                                 _Output_
 /*
-Welcome to Java.
-Welcome to Java.
-Welcome to Java.
-Welcome to Java.
-Welcome to Java.
+Welcome to Java
+Welcome to Java
+Welcome to Java
+Welcome to Java
+Welcome to Java
  */
