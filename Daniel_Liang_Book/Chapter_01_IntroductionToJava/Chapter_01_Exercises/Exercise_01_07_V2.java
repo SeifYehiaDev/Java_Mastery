90e1a8f872efe037403dc11f4e3075242fac64c4
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2025-07-19 20:27:59 +0300

    Approximate PI

--- Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_07_V2.java
+++ Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_07_V2.java
@@ -2,24 +2,27 @@
  * Author: Seif Yehia
  * Socials: SeifYehiaDev (Facebook, LinkedIn, Instagram, GitHub)
  * Created: 05/07/2025
- * Purpose: Java learning journey — practice from book and course
  */
 
 package Chapter_01_Exercises;
 
 public class Exercise_01_07_V2 {
     public static void main(String[] args) {
+        System.out.println("Approximate PI for I = 11: " + computePI(11));
+        System.out.print("Approximate PI for I = 13: " + computePI(13));
+    }
+
+    public static double computePI(final int value) {
         double sum = 0.0;
         int sign = 1;
-        for (int number = 1; number <= 13; number += 2) {
-            sum += sign * (1.0 / number);
-            sign *= -1;
-        }
-        System.out.print("PI = " + 4 * sum);
+        for (int index = 1; index <= value; sign *= -1, index += 2)
+            sum += sign * (1.0 / index);
+        return 4 * sum;
     }
 }
 
 //                                                 _Output_
 /*
-PI = 3.2837384837384844
+Approximate PI for I = 11: 2.9760461760461765
+Approximate PI for I = 13: 3.2837384837384844
  */
