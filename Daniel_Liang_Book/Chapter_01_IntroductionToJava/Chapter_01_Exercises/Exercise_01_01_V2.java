e0c311dc388f063f82101c400854ce00c5d6a286
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2025-07-19 20:24:51 +0300

    Display three messages

--- Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_01_V2.java
+++ Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_01_V2.java
@@ -2,7 +2,6 @@
  * Author: Seif Yehia
  * Socials: SeifYehiaDev (Facebook, LinkedIn, Instagram, GitHub)
  * Created: 05/07/2025
- * Purpose: Java learning journey — practice from book and course
  */
 
 package Chapter_01_Exercises;
@@ -10,15 +9,15 @@ package Chapter_01_Exercises;
 public class Exercise_01_01_V2 {
     public static void main(String[] args) {
         System.out.print("""
-                Welcome to Java.
-                Welcome to Computer Science.
-                Programming is fun.""");
+                Welcome to Java
+                Welcome to Computer Science
+                Programming is fun""");
     }
 }
 
 //                                                 _Output_
 /*
-Welcome to Java.
-Welcome to Computer Science.
-Programming is fun.
+Welcome to Java
+Welcome to Computer Science
+Programming is fun
  */
