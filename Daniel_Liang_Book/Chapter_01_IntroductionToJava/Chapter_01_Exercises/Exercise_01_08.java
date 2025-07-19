887e1bf10a5d2a0788334115f257b61bc239e8fc
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2025-07-19 20:28:16 +0300

    Area and perimeter of a circle

--- Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_08.java
+++ Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_08.java
@@ -2,7 +2,6 @@
  * Author: Seif Yehia
  * Socials: SeifYehiaDev (Facebook, LinkedIn, Instagram, GitHub)
  * Created: 05/07/2025
- * Purpose: Java learning journey — practice from book and course
  */
 
 package Chapter_01_Exercises;
@@ -11,8 +10,8 @@ public class Exercise_01_08 {
     public static void main(String[] args) {
         final double RADIUS = 5.5;
         System.out.printf("""
-                The perimeter of a circle is %f
-                The area of a circle is %f""",
+                        The perimeter of a circle is %f
+                        The area of a circle is %f""",
                 2 * Math.PI * RADIUS, Math.pow(RADIUS, 2.0) * Math.PI);
     }
 }
