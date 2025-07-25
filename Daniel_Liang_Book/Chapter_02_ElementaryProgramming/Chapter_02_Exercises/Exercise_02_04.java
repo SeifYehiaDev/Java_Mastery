206d748a23602f14e90369f4cd6d8e7e82069d48
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2025-07-25 21:01:21 +0300

    Convert pounds into kilograms

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Exercises/Exercise_02_04.java
@@ -0,0 +1,27 @@
+/*
+ * Author: Seif Yehia
+ * Socials: SeifYehiaDev (Facebook, LinkedIn, Instagram, GitHub)
+ * Created: 25/07/2025
+ */
+
+package Chapter_02_Exercises;
+
+import java.util.Scanner;
+
+public class Exercise_02_04 {
+    public static void main(String[] args) {
+        final double KILOGRAMS_PER_POUND = 0.454;
+        double pounds, kilograms;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number in pounds: ");
+        pounds = input.nextDouble();
+        kilograms = pounds * KILOGRAMS_PER_POUND;
+        System.out.printf("%.1f pounds is %.3f kilograms", pounds, kilograms);
+    }
+}
+
+//                                                 _Output_
+/*
+Enter a number in pounds: 55.5
+55.5 pounds is 25.197 kilograms
+ */
