7f11aecec9783bffb41b6d337783e6c06fa45528
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2025-07-25 21:03:47 +0300

    Health application: computing BMI

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Exercises/Exercise_02_14_V1.java
@@ -0,0 +1,32 @@
+/*
+ * Author: Seif Yehia
+ * Socials: SeifYehiaDev (Facebook, LinkedIn, Instagram, GitHub)
+ * Created: 25/07/2025
+ */
+
+package Chapter_02_Exercises;
+
+import java.util.Scanner;
+
+public class Exercise_02_14_V1 {
+    public static void main(String[] args) {
+        final double KILOGRAMS_PER_POUND = 0.45359237D, METERS_PER_INCH = 0.0254D;
+        double weightInPounds, heightInInches, weightInKilograms, heightInMeters, bmi;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter weight in pounds: ");
+        weightInPounds = input.nextDouble();
+        System.out.print("Enter height in inches: ");
+        heightInInches = input.nextDouble();
+        weightInKilograms = weightInPounds * KILOGRAMS_PER_POUND;
+        heightInMeters = heightInInches * METERS_PER_INCH;
+        bmi = weightInKilograms / Math.pow(heightInMeters, 2.0);
+        System.out.printf("BMI is %.4f", bmi);
+    }
+}
+
+//                                                 _Output_
+/*
+Enter weight in pounds: 95.5
+Enter height in inches: 50
+BMI is 26.8573
+ */
